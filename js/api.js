f9482c49283d07c5d7bba86d3869892e63296853
FernandoHN99 <nandohneto@gmail.com>
2026-02-09 17:42:48 -0300

    Separando bakcend do frontend

--- js/api.js
+++ js/api.js
@@ -1,7 +1,6 @@
 export async function fetchMetrics() {
   try {
-    const response = await fetch('https://raw.githubusercontent.com/FernandoHN99/btc-metrics/main/metrics.jsonl', { cache: 'no-store' })
-    console.log('response: ', response)
+    const response = await fetch('https://raw.githubusercontent.com/FernandoHN99/btc-metrics/database/metrics.jsonl', { cache: 'no-store' })
     if (!response.ok) throw new Error(`HTTP ${response.status}`);
 
     const text = await response.text();

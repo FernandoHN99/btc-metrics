82a7c1d8ab8ec2f414d704aeeaec2a6dbe19998d
FernandoHN99 <nandohneto@gmail.com>
2026-02-09 15:17:21 -0300

    ajustando interface score

--- frontend/js/metrics.js
+++ frontend/js/metrics.js
@@ -1,15 +1,18 @@
 export function updateMetrics(latest) {
-  if (!latest) return;
+    if (!latest) return;
 
-  document.getElementById('metric-price').textContent =
-    `$${latest.price.toLocaleString('en-US')}`;
+    document.getElementById('metric-price').textContent =
+        `$${latest.price.toLocaleString('en-US')}`;
 
-  document.getElementById('metric-mvrv').textContent =
-    latest.mvrv.toFixed(2);
+    document.getElementById('metric-mvrv').textContent =
+        latest.mvrv.toFixed(2);
 
-  document.getElementById('metric-mayer').textContent =
-    latest.mayer.toFixed(2);
+    document.getElementById('metric-mayer').textContent =
+        latest.mayer.toFixed(2);
 
-  document.getElementById('metric-fg').textContent =
-    `${latest.fear_greed.value} – ${latest.fear_greed.classification}`;
+    document.getElementById('metric-fg').textContent =
+        `${latest.fear_greed.value} – ${latest.fear_greed.classification}`;
+
+    document.getElementById('last-update-datetime').textContent =
+        `Última Atualização: ${new Date(latest.date).toLocaleString('pt-BR')}`;
 }
